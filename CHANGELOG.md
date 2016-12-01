bb615e404f2fdeeeaadda4cb1e880b53cbf55234
Aaron Collegeman <aaron@collegeman.net>
2016-12-01 12:59:36 -0500

    v1.0.1

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# CHANGELOG
+
+## v1.0.1
+
+* Fix a bug in the autoloader context detection
+
+## v1.0.0
+
+* First release. Yay!
--- bootstrap.php
+++ bootstrap.php
@@ -12,7 +12,9 @@ Text Domain: 		fp-auth
 Domain Path: 		/resources/lang
 */
 
-require_once __DIR__.'/vendor/autoload.php';
+if (file_exists( __DIR__.'/vendor/autoload.php' )) {
+	require_once( __DIR__.'/vendor/autoload.php' );
+}
 require_once __DIR__.'/src/functions.php';
 require_once __DIR__.'/src/plugin.php';
 
